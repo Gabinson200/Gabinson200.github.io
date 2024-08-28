ce40122a3686bffaacaec231e98a56585e6febaa
Gabinson200 <42674620+Gabinson200@users.noreply.github.com>
2024-08-27 21:46:17 -0700

    less max particles

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let isPopupOpen = false;
 let nodeColorAnimationId;
 
 // Constants
-const PARTICLE_COUNT = 1000;
+const PARTICLE_COUNT = 100;
 const LAYER_SIZES = [1, 2, 3, 1, 1];
 const LAYER_SPACING = 1.5;
 
